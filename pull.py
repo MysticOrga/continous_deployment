0fab285515d13926845937dcf4d3260c7023dbdc
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-12-11 13:53:50 +0400

    CD: test new continous deployment

---
+++ pull.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+from requests import get
+from os import getenv
+from sys import exit
+
+NO_CHANGE = 0
+CHANGE = 1
+def get_sha() -> str:
+    resp = get(
+        f"{GITHUB_API_URL}/{OWNER}/{REPO}/commits/{REF}",
+        headers=HEADER
+    )
+    return resp._content.decode('utf-8')
+
+def check_sha(sha: str) -> bool:
+    f = open('./last_sha.txt', 'r+')
+    if sha != f.read():
+        print("Changed, need to pull last change.")
+        f.seek(0)
+        f.write(sha)
+        f.truncate()
+        f.close()
+        return True
+    else:
+        print("Nothing Change.")
+        f.close()
+        return False
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    GITHUB_ACCESS_TOKEN = getenv('GITHUB_ACCESS_TOKEN')
+    OWNER = getenv('OWNER')
+    REPO = getenv('REPO')
+    REF = getenv('REF')
+    GITHUB_API_URL = 'https://api.github.com/repos'
+    HEADER = {
+        "Accept": "application/vnd.github.sha",
+        "Authorization": f"Bearer {GITHUB_ACCESS_TOKEN}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    sha: str = get_sha()
+    print(f"get new sha: {sha}")
+    exit(CHANGE) if check_sha(sha) == True else exit(NO_CHANGE)
